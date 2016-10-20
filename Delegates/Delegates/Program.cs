531ce6c2c824c0e747421ca9f0ebfa0b1108078b
Anton <Anton@ANTON>
2016-10-20 16:03:32 +0500

    TProcessor

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TProcessor().Process(new TransactionRequest(), x => true, y => new Transaction(), x => Console.WriteLine(x.GetType()));
+            new TProcessor().Process(new OrderRequest(), x => true, y => new Order(), x => Console.WriteLine(x.GetType()));
+            Console.Read();
+        }
+    }
+}
---
+++ Delegates/Delegates/TProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class TProcessor
+    {
+        public T Process<T, TRequest>(TRequest request, Func<TRequest, bool> check, Func<TRequest, T> register, Action<T> save)
+        {
+            if (!check(request))
+                throw new ArgumentException();
+            var result = register(request); 
+            save(result);
+            return result; 
+        }
+    }
+
+    public class TransactionRequest
+    {
+    }
+
+    public class Transaction
+    {
+    }
+
+    public class OrderRequest
+    {
+    }
+
+    public class Order
+    {
+    }
+}
